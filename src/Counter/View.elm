aa8942e2e4348fe5f6f0035959e6554132d2ed54
wyi <wei.yi@ubisoft.com>
2018-11-28 17:38:46 +0800

    Removing elm-ui from View

--- src/Counter/View.elm
+++ src/Counter/View.elm
@@ -1,17 +1,16 @@
 module Counter.View exposing (view)
 
 import Counter.Types exposing (..)
-import Element exposing (..)
-import Element.Events exposing (..)
-import Element.Input exposing (button)
+import Html exposing (Html, button, text, div)
+import Html.Events exposing (onClick)
 import RemoteData exposing (..)
+import Types exposing (Msg(..))
 import Styles
 
 
-view : Model -> Element Msg
+view : Model -> Html Msg
 view model =
-    row [ centerY, spacing 5 ]
-        [ button ([ padding 5 ] ++ Styles.button) { onPress = Just Decrement, label = text "-" }
-        , text (String.fromInt model)
-        , button ([ padding 5 ] ++ Styles.button) { onPress = Just Increment, label = text "+" }
+    div []
+        [
+            button [onClick (MsgForCounter Increment)] [text "+"]
         ]
--- src/View.elm
+++ src/View.elm
@@ -1,12 +1,10 @@
 module View exposing (renderRoute, view)
 
 import Browser
+import Html exposing (Html, div, button, text)
+import Html.Attributes exposing (class)
 import Cats.View
 import Counter.View
-import Element exposing (..)
-import Element.Events exposing (..)
-import Element.Input exposing (button)
-import Element.Region exposing (..)
 import Router.Routes exposing (..)
 import Router.Types exposing (Msg(..))
 import Styles
@@ -16,26 +14,30 @@ import Types exposing (..)
 view : Model -> Browser.Document Types.Msg
 view model =
     { title = "expectit"
-    , body =
-        [ Element.layout [] <|
-            el [ width (px 800), centerX ] (renderRoute model)
+    , body = [
+        div []
+            [
+                div [class "red-text"] [(renderRoute model)]
+            ]
         ]
     }
 
 
-renderRoute : Model -> Element Types.Msg
+renderRoute : Model -> Html Types.Msg
 renderRoute model =
     case model.router.page of
         Home ->
-            column
-                [ spacing 5 ]
-                [ el ([ heading 1 ] ++ Styles.title) (text "Welcome")
-                , row [ spacing 5 ]
-                    [ link ([ padding 5 ] ++ Styles.button) { url = toPath CatsPage, label = text "Go to Cats" }
-                    , link ([ padding 5 ] ++ Styles.button) { url = toPath CounterPage, label = text "Go to Counter" }
-                    ]
-                ]
+            div [] []
 
+        NotFound ->
+            text "404 Not Found"
+
+        CatsPage ->
+            div [] [text "Cats"]
+
+        CounterPage ->
+            Counter.View.view model.counter
+{--
         NotFound ->
             text "404 Not Found"
 
@@ -44,3 +46,4 @@ renderRoute model =
 
         CounterPage ->
             Element.map MsgForCounter (Counter.View.view model.counter)
+--}
