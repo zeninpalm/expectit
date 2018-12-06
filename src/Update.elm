5aefb1b94dc446231f3e94d6f44075c887af689c
wyi <wei.yi@ubisoft.com>
2018-12-06 16:03:38 +0800

    Add comments to help readers to understand code

--- src/Update.elm
+++ src/Update.elm
@@ -21,10 +21,19 @@ init _ url key =
 update : Msg -> Model -> Return Msg Model
 update msg model =
     singleton Model
+        -- andMapCmd : (msg1 ->msg2)  ->   Return Msg Model
         |> andMapCmd MsgForRouter (Router.Update.update msg model.router)
         |> andMapCmd MsgForCats (Cats.Update.update msg model.cats)
         |> andMapCmd MsgForCounter (Counter.Update.update msg model.counter)
 
+-- Simply deduced from type signature, this function may be replaced by this implementation
+{-
+
+andMapCmd : (msg1 -> msg2) -> (model1 -> model2) -> Return msg1 model1 -> Return msg2 model2
+andMapCmd msgTransform modelTransform = mapBoth msgTransform modelTransform
+
+But here we don't have (model1 -> model2) function, so we the function below
+-}
 
 andMapCmd : (msg1 -> msg2) -> Return msg1 model1 -> Return msg2 (model1 -> model2) -> Return msg2 model2
 andMapCmd msg =
